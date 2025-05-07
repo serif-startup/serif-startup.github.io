4da1555fea44f9bce37bdefdf898aa3858f67df1
Serif Organization <serif_startup@protonmail.com>
2025-05-07 17:12:36 +0200

    updated homepage

--- vite.config.ts
+++ vite.config.ts
@@ -14,7 +14,6 @@ export default defineConfig(({ mode }) => ({
     mode === 'development' &&
     componentTagger(),
   ].filter(Boolean),
-  base: "/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
